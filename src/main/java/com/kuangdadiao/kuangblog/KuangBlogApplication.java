ddc3472cf24ac003d27b8239d02243a3cc23e5e2
DesNevermore <826086142@qq.com>
2021-05-21 01:22:37 +0800

    init: project initialized

---
+++ src/main/java/com/kuangdadiao/kuangblog/KuangBlogApplication.java
@@ -0,0 +1,13 @@
+package com.kuangdadiao.kuangblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuangBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuangBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuangdadiao/kuangblog/KuangBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuangdadiao.kuangblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuangBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
